48dde5859ae6321ffe56bc43604f047f952e9e4b
josiepollard <55194487+josiepollard@users.noreply.github.com>
2019-10-14 15:22:45 +0100

    Add files via upload

---
+++ IntroWeb/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+  "I hate green turtles!!!"
+end
+
+get '/cat' do
+  "<div style= 'border: 3px dashed red;'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
