c30ada6a4f66b2f86a29afde156a3c6de4fbc1a5
Will <william.bath@btconnect.com>
2019-10-15 13:45:46 +0100

    Added /bristol_cat

--- IntroWeb/app.rb
+++ IntroWeb/app.rb
@@ -9,13 +9,18 @@ get '/secret' do
 end
 
 get '/random_cat' do
-  @cat_name = ["Josie", "Will", "Albion", "Toby", "Luke"].sample
+  @cat_name = ['Josie', 'Will', 'Albion', 'Toby', 'Luke'].sample
   erb(:cat)
 end
 
 get '/named_cat' do
   p params
-  @cat_name = (params[(:name)])
+  @cat_name = (params[:name])
   p @cat_name
   erb(:cat)
+end
+
+get '/bristol_cat' do
+  @cat_name = params[:name]
+  erb(:bristol_cat)
 end
